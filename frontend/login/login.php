8805cac7d48df8e59b1c726820ac899fd7484293
Axel Perez <152705784+axelprz@users.noreply.github.com>
2024-08-20 19:46:41 -0300

    Adding basic structure

---
+++ frontend/login/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script
+    src="https://kit.fontawesome.com/81448e9ee5.js"
+    crossorigin="anonymous"></script>
+  <title>Tractores Clavijo</title>
+  <link rel="stylesheet" href="styles.css" />
+</head>
+
+<body>
+  <header>
+    <h2 class="logo">Tractores Clavijo</h2>
+    <nav class="navegacion">
+      <a href="../main/main.php">Inicio</a>
+      <a href="../about/about.php">Informacion</a>
+      <a href="../tractors/tractors.php">Tractores</a>
+      <a href="../contact/contact.php">Contactos</a>
+    </nav>
+  </header>
+
+  <div class="fondo">
+    <span class="icono-cerrar"><i class="fa-solid fa-xmark"></i></span>
+
+    <div class="contenedor-form"></div>
+  </div>
+</body>
+
+</html>
---
+++ frontend/main/main.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script
+    src="https://kit.fontawesome.com/81448e9ee5.js"
+    crossorigin="anonymous"></script>
+  <title>Tractores Clavijo</title>
+  <link rel="stylesheet" href="styles.css" />
+</head>
+
+<body>
+  <header>
+    <h2 class="logo">Tractores Clavijo</h2>
+
+    <nav class="navegacion">
+      <a href="../main/main.php">Inicio</a>
+      <a href="../about/about.php">Informacion</a>
+      <a href="../tractors/tractors.php">Tractores</a>
+      <a href="../contact/contact.php">Contactos</a>
+      <form action="../login/login.php" method="get">
+        <button type="submit" class="btn">Iniciar Sesion</button>
+      </form>
+    </nav>
+  </header>
+</body>
+
+</html>
